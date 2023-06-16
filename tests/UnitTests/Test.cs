bd2ae2ad80954d98b4b08d330f041d6e3d21ea17
Nelson Neto <nelson.ash@outlook.com>
2023-06-16 23:22:31 +0000

    Criacao de Projeto de Testes Unitarios

---
+++ tests/UnitTests/Test.cs
@@ -0,0 +1,15 @@
+namespace Pragma.UnitTests;
+
+public class Test
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Fail();
+    }
+}
