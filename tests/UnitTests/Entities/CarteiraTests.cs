f05b1020225df5317892853bb9a0dbc14055d7c5
Nelson Netoe <nelson.ash@outlook.com>
2023-06-18 07:50:56 -0300

    Refactor

--- tests/UnitTests/Entities/CarteiraTests.cs
+++ tests/UnitTests/Entities/CarteiraTests.cs
@@ -11,35 +11,36 @@ public class CarteiraTests
     public void Setup()
     {
         carteira = new Carteira();
+        carteira.Depositar(10M);
     }
 
     [Test]
     public void Criar_Carteira_Com_Sucesso()
     {
         Assert.That(carteira.Ativos.Count, Is.EqualTo(0));
+        Assert.That(carteira.Saldo, Is.EqualTo(10M));
     }
+
     [Test]
     public void Comprar_Acao_Com_Sucesso()
     {
-        decimal valorAcao = 10M;
-        decimal saldoDisponivelEmCarteira = 30M;
+        decimal valorAcao = 5M;
         Ativo acao = new Ativo("Qualquer Ação",TipoAtivoEnum.B3, valorAcao);
-        carteira.Depositar(saldoDisponivelEmCarteira);
         carteira.Comprar(acao);
         Assert.That(carteira.Ativos.Count, Is.EqualTo(1));
-        Assert.That(carteira.Saldo, Is.EqualTo(20M));
+        Assert.That(carteira.Saldo, Is.EqualTo(5M));
     }
+
     [Test]
     public void Comprar_Acao_Com_Erro()
     {
         //Arrange
-        decimal valorAcao = 10M;
-        decimal saldoDisponivelEmCarteira = 5M;
+        decimal valorAcao = 15M;
         //Act
         Ativo acao = new Ativo("Qualquer Ação",TipoAtivoEnum.B3, valorAcao);
-        carteira.Depositar(saldoDisponivelEmCarteira);
         //Assert
         Assert.That(() => carteira.Comprar(acao), Throws.Exception);
         Assert.That(carteira.Ativos.Count, Is.EqualTo(0));
+        Assert.That(carteira.Saldo, Is.EqualTo(10));
     }
 }
